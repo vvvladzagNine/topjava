b5ed62c77908a22a6b9dd49af7b988af0999023e
Vladislav Zagidullin <wladzag999@gmail.com>
2019-08-07 13:16:21 +0500

    8_7_8_ajax_refactor_js

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -17,9 +17,20 @@ public abstract class AbstractUserController {
     @Autowired
     protected UserService service;
 
+    public List<User> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
     public User get(int id) {
         log.info("get {}", id);
         return service.get(id);
+    }
+
+    public User create(User user) {
+        log.info("create {}", user);
+        checkNew(user);
+        return service.create(user);
     }
 
     public void delete(int id) {
