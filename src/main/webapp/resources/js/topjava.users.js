083e42455e9ef63591953bacfa430582d039c7c9
Vladislav Zagidullin <wladzag999@gmail.com>
2019-08-15 12:26:48 +0500

    9_5_update

--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -1,6 +1,3 @@
-var ajaxUrl = "ajax/admin/users/";
-var datatableApi;
-
 const userAjaxUrl = "ajax/admin/users/";
 
 function enable(chkbox, id) {
@@ -12,7 +9,7 @@ function enable(chkbox, id) {
         data: "enabled=" + enabled
     }).done(function () {
         chkbox.closest("tr").attr("data-userEnabled", enabled);
-        successNoty(enabled ? "Enabled" : "Disabled");
+        successNoty(enabled ? "common.enabled" : "common.disabled");
     }).fail(function () {
         $(chkbox).prop("checked", !enabled);
     });
@@ -20,40 +17,75 @@ function enable(chkbox, id) {
 
 // $(document).ready(function () {
 $(function () {
-    datatableApi = $("#datatable").DataTable({
-        "paging": false,
-        "info": true,
-        "columns": [
-            {
-                "data": "name"
-            },
-            {
-                "data": "email"
-            },
-            {
-                "data": "roles"
-            },
-            {
-                "data": "enabled"
-            },
-            {
-                "data": "registered"
-            },
-            {
-                "defaultContent": "Edit",
-                "orderable": false
-            },
-            {
-                "defaultContent": "Delete",
-                "orderable": false
+    makeEditable({
+            ajaxUrl: userAjaxUrl,
+            datatableApi: $("#datatable").DataTable({
+                "ajax": {
+                    "url": userAjaxUrl,
+                    "dataSrc": ""
+                },
+                "paging": false,
+                "info": true,
+                "columns": [
+                    {
+                        "data": "name"
+                    },
+                    {
+                        "data": "email",
+                        "render": function (data, type, row) {
+                            if (type === "display") {
+                                return "<a href='mailto:" + data + "'>" + data + "</a>";
+                            }
+                            return data;
+                        }
+                    },
+                    {
+                        "data": "roles"
+                    },
+                    {
+                        "data": "enabled",
+                        "render": function (data, type, row) {
+                            if (type === "display") {
+                                return "<input type='checkbox' " + (data ? "checked" : "") + " onclick='enable($(this)," + row.id + ");'/>";
+                            }
+                            return data;
+                        }
+                    },
+                    {
+                        "data": "registered",
+                        "render": function (date, type, row) {
+                            if (type === "display") {
+                                return date.substring(0, 10);
+                            }
+                            return date;
+                        }
+                    },
+                    {
+                        "orderable": false,
+                        "defaultContent": "",
+                        "render": renderEditBtn
+                    },
+                    {
+                        "orderable": false,
+                        "defaultContent": "",
+                        "render": renderDeleteBtn
+                    }
+                ],
+                "order": [
+                    [
+                        0,
+                        "asc"
+                    ]
+                ],
+                "createdRow": function (row, data, dataIndex) {
+                    if (!data.enabled) {
+                        $(row).attr("data-userEnabled", false);
+                    }
+                }
+            }),
+            updateTable: function () {
+                $.get(userAjaxUrl, updateTableByData);
             }
-        ],
-        "order": [
-            [
-                0,
-                "asc"
-            ]
-        ]
-    });
-    makeEditable();
+        }
+    );
 });
